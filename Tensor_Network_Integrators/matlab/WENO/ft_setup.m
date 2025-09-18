d6fb64174a33ff23f5562867f831c79ff3373d97
Oleg Korobkin <korobkin@lanl.gov>
2025-09-18 13:55:08 -0600

    (*) paths in WENO/tt_setup.m and ft_setup.m

--- Tensor_Network_Integrators/matlab/WENO/ft_setup.m
+++ Tensor_Network_Integrators/matlab/WENO/ft_setup.m
@@ -6,7 +6,7 @@ fdwenolib.srcdir = fdwenolib.basedir + "/src";
 fdwenolib.tt    = fdwenolib.srcdir + "/tt";
 fdwenolib.ft    = fdwenolib.srcdir + "/ft";
 fdwenolib.misc  = fdwenolib.srcdir + "/misc";
-fdwenolib.ttlib = fileparts(fdwenolib.basedir)+"/external-libs/TT-toolbox";
+fdwenolib.ttlib = fileparts(fdwenolib.basedir)+"../utils/TT-toolbox";
 %
 addpath(fdwenolib.ft);
 addpath(fdwenolib.misc);
